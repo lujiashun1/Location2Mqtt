3b0eb1b2002e8cd1ac416a217ef09b0dd1f230b2
lujiashun1 <531894004@qq.com>
2021-01-14 11:40:53 +0800

    增加地理位置

--- app/src/main/java/com/ljs/location2mqtt/ltmService.java
+++ app/src/main/java/com/ljs/location2mqtt/ltmService.java
@@ -192,9 +192,9 @@ public class ltmService extends Service {
         }
         //mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
         mOption.setGpsFirst(true);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        mOption.setHttpTimeOut(120000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+        mOption.setHttpTimeOut(600000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
         mOption.setInterval(time*1000);//可选，设置定位间隔。默认为2秒
-        mOption.setNeedAddress(false);//可选，设置是否返回逆地理地址信息。默认是true
+        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
         mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
         mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
         AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
@@ -222,13 +222,27 @@ public class ltmService extends Service {
                     double lat = location.getLatitude();
                     double lon = location.getLongitude();
                     float acc = location.getAccuracy();
-                    //double alt = location.getAltitude();
+                    double alt = location.getAltitude();
                     HashMap<String, Double> hm = delta(lat,lon);
                     lat=hm.get("lat");
                     lon=hm.get("lon");
                     BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
                     int battery = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-                    String str = "{\"latitude\":" + lat + ",\"longitude\":" + lon + ",\"gps_accuracy\":" + acc  + ",\"battery\":" + battery + "}";
+                    float speed = location.getSpeed();
+                    float bearing = location.getBearing();
+                    int satellites = location.getSatellites();
+                    String country = location.getCountry();
+                    String province = location.getProvince();
+                    String city = location.getCity();
+                    String cityCode = location.getCityCode();
+                    String district = location.getDistrict();
+                    String street = location.getStreet();
+                    String streetNum = location.getStreetNum();
+                    String adCode = location.getAdCode();
+                    String address = location.getAddress();
+                    String poiName = location.getPoiName();
+                    //String str = "{\"latitude\":" + lat + ",\"longitude\":" + lon + ",\"gps_accuracy\":" + acc  + ",\"battery\":" + battery + "}";
+                    String str="{    \"latitude\":"+lat+",    \"longitude\":"+lon+",    \"altitude\":"+alt+",    \"gps_accuracy\":"+acc+",    \"battery\":"+battery+",    \"info\":{        \"speed\":"+speed+",        \"bearing\":"+bearing+",        \"satellites\":"+satellites+",        \"country\":\""+country+"\",        \"province\":\""+province+"\",        \"city\":\""+city+"\",        \"citycode\":\""+cityCode+"\",        \"district\":\""+district+"\",        \"adcode\":\""+adCode+"\",\"street\":\""+street+"\", \"streetnum\":\""+streetNum+"\",       \"address\":\""+address+"\",        \"poiname\":\""+poiName+"\"    }}";
                     msg.setPayload(str.getBytes());
                     msg.setQos(0);
                     msg.setRetained(true);
@@ -266,7 +280,8 @@ public class ltmService extends Service {
 //                    sb.append("地    址    : " + location.getAddress() + "\n");
 //                    sb.append("兴趣点    : " + location.getPoiName() + "\n");
 //                    //定位完成的时间
-//                    sb.append("定位时间: " + formatUTC(location.getTime(), "yyyy-MM-dd HH:mm:ss") + "\n");
+//                    //sb.append("定位时间: " + formatUTC(location.getTime(), "yyyy-MM-dd HH:mm:ss") + "\n");
+//                    Toast.makeText(getApplicationContext(),sb.toString(),Toast.LENGTH_LONG).show();
                 } else {
 
                     //定位失败
